1230659391b65139f4a8205bd3525a945fd660ef
Imran Qureshi <imranq2@hotmail.com>
2025-07-18 08:29:47 -0700

    add a test for confluence (not working)

---
+++ tests/gateway/mcp_tools/test_chat_anthropic_with_mcp_confluence.py
@@ -0,0 +1,89 @@
+from typing import Optional, List
+
+import httpx
+import pytest
+from openai import AsyncOpenAI
+from openai.types.chat import ChatCompletion, ChatCompletionUserMessageParam
+
+from language_model_gateway.configs.config_schema import (
+    ChatModelConfig,
+    ModelConfig,
+    AgentConfig,
+)
+from language_model_gateway.container.simple_container import SimpleContainer
+from language_model_gateway.gateway.api_container import get_container_async
+from language_model_gateway.gateway.models.model_factory import ModelFactory
+from language_model_gateway.gateway.utilities.environment_reader import (
+    EnvironmentReader,
+)
+from language_model_gateway.gateway.utilities.expiring_cache import ExpiringCache
+from tests.gateway.mocks.mock_chat_model import MockChatModel
+from tests.gateway.mocks.mock_model_factory import MockModelFactory
+
+
+@pytest.mark.asyncio
+async def test_chat_completions_with_mcp_confluence(
+    async_client: httpx.AsyncClient,
+) -> None:
+    print("")
+    test_container: SimpleContainer = await get_container_async()
+    if not EnvironmentReader.is_environment_variable_set("RUN_TESTS_WITH_REAL_LLM"):
+        test_container.register(
+            ModelFactory,
+            lambda c: MockModelFactory(
+                fn_get_model=lambda chat_model_config: MockChatModel(
+                    fn_get_response=lambda messages: "Donald Trump won the last US election"
+                )
+            ),
+        )
+
+    # set the model configuration for this test
+    model_configuration_cache: ExpiringCache[List[ChatModelConfig]] = (
+        test_container.resolve(ExpiringCache)
+    )
+    await model_configuration_cache.set(
+        [
+            ChatModelConfig(
+                id="general_purpose",
+                name="General Purpose",
+                description="General Purpose Language Model",
+                type="langchain",
+                model=ModelConfig(
+                    provider="bedrock",
+                    model="us.anthropic.claude-3-5-haiku-20241022-v1:0",
+                ),
+                tools=[
+                    AgentConfig(
+                        name="mcp_github_search",
+                        url="https://mcp.atlassian.com/v1/sse",
+                        # headers={"Authorization": "Bearer $GITHUB_TOKEN"},
+                    ),
+                ],
+            )
+        ]
+    )
+
+    # init client and connect to localhost server
+    client = AsyncOpenAI(
+        api_key="fake-api-key",
+        base_url="http://localhost:5000/api/v1",  # change the default port if needed
+        http_client=async_client,
+    )
+
+    # call API
+    message: ChatCompletionUserMessageParam = {
+        "role": "user",
+        "content": "list last 2 changed Confluence pages in icanbwell organization",
+    }
+    chat_completion: ChatCompletion = await client.chat.completions.create(
+        messages=[message],
+        model="General Purpose",
+    )
+
+    # print the top "choice"
+    content: Optional[str] = "\n".join(
+        choice.message.content or "" for choice in chat_completion.choices
+    )
+    assert content is not None
+    print(content)
+    assert "fhir" in content
