fef2f4890f5a8a1b375ebafcfd7663fba269adb8
Imran Qureshi <imranq2@hotmail.com>
2025-07-28 19:18:27 -0700

    cache metadata for tools

--- language_model_gateway/gateway/langchain_overrides/multiserver_mcp_client_with_caching.py
+++ language_model_gateway/gateway/langchain_overrides/multiserver_mcp_client_with_caching.py
@@ -1,3 +1,4 @@
+import logging
 from typing import override, List, Dict
 
 from langchain_core.tools import BaseTool
@@ -11,10 +12,23 @@ from langchain_mcp_adapters.tools import (
 )
 from mcp import ClientSession, Tool
 
+logger = logging.getLogger(__name__)
+
 
 class MultiServerMCPClientWithCaching(MultiServerMCPClient):  # type: ignore[misc]
-    @override
-    async def get_tools(self, *, server_name: str | None = None) -> list[BaseTool]:
+    """A MultiServerMCPClient that caches tool metadata to avoid repeated calls to the MCP server.
+
+    This class extends the MultiServerMCPClient to cache the metadata of tools
+    across multiple calls to `get_tools`. It allows for efficient retrieval of tools
+    without needing to repeatedly query the MCP server for the same tool metadata.
+    """
+
+    all_tools_metadata_cache: Dict[str, list[Tool]] = {}
+    """Cache for all tools metadata across all servers.  url is connection URL of MCP server."""
+
+    async def load_tools_metadata_cache(
+        self, *, server_name: str | None = None
+    ) -> None:
         """Get a list of all tools from all connected servers.
 
         Args:
@@ -31,24 +45,56 @@ class MultiServerMCPClientWithCaching(MultiServerMCPClient):  # type: ignore[mis
             if server_name not in self.connections:
                 msg = f"Couldn't find a server with name '{server_name}', expected one of '{list(self.connections.keys())}'"
                 raise ValueError(msg)
-            return self.create_tools_from_list(
-                await self.load_metadata_for_mcp_tools(
-                    None, connection=self.connections[server_name]
-                ),
-                session=None,
-                connection=self.connections[server_name],
-            )
+            connection_for_server = self.connections[server_name]
+            if connection_for_server["url"] not in self.all_tools_metadata_cache:
+                self.all_tools_metadata_cache[
+                    connection_for_server["url"]
+                ] = await self.load_metadata_for_mcp_tools(
+                    None, connection=connection_for_server
+                )
+                logger.info(
+                    f"Loaded tools for connection {connection_for_server['url']}"
+                )
+            else:
+                logger.debug(
+                    f"Tools for connection {connection_for_server['url']} are already cached"
+                )
+        else:
+            for connection in self.connections.values():
+                # if the tools for this connection are already cached, skip loading them
+                if connection["url"] not in self.all_tools_metadata_cache:
+                    self.all_tools_metadata_cache[
+                        connection["url"]
+                    ] = await self.load_metadata_for_mcp_tools(
+                        None, connection=connection
+                    )
+                    logger.info(f"Loaded tools for connection {connection['url']}")
+                else:
+                    logger.debug(
+                        f"Tools for connection {connection['url']} are already cached"
+                    )
 
-        all_tools_metadata: Dict[str, list[Tool]] = {}
-        for connection in self.connections.values():
-            all_tools_metadata[
-                connection["url"]
-            ] = await self.load_metadata_for_mcp_tools(None, connection=connection)
+    @override
+    async def get_tools(self, *, server_name: str | None = None) -> list[BaseTool]:
+        """Get a list of all tools from all connected servers.
+
+        Args:
+            server_name: Optional name of the server to get tools from.
+                If None, all tools from all servers will be returned (default).
+
+        NOTE: a new session will be created for each tool call
+
+        Returns:
+            A list of LangChain tools
+
+        """
+
+        await self.load_tools_metadata_cache(server_name=server_name)
 
         # create LangChain tools from the loaded MCP tools
         all_tools: List[BaseTool] = []
         for connection in self.connections.values():
-            tools_for_connection = all_tools_metadata[connection["url"]]
+            tools_for_connection = self.all_tools_metadata_cache[connection["url"]]
             all_tools.extend(
                 self.create_tools_from_list(
                     tools_for_connection, session=None, connection=connection
@@ -94,6 +140,13 @@ class MultiServerMCPClientWithCaching(MultiServerMCPClient):  # type: ignore[mis
         session: ClientSession | None = None,
         connection: Connection | None = None,
     ) -> List[BaseTool]:
+        """
+        Create LangChain tools from a list of MCP tools.
+        Args:
+            tools: List of MCP tools to convert.
+            session: The MCP client session. If None, connection must be provided.
+            connection: Connection config to create a new session if session is None.
+        """
         return [
             convert_mcp_tool_to_langchain_tool(session, tool, connection=connection)
             for tool in tools
